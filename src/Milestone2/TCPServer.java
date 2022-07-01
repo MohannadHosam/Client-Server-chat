e101ea7e1b55a866f9fada9b997f28142f953e4a
MohannadHosam <mohannad.hosam17@gmail.com>
2022-07-01 23:41:27 +0200

    First Commit

---
+++ src/Milestone2/TCPServer.java
@@ -0,0 +1,29 @@
+package Milestone2;
+
+import java.io.*; 
+import java.net.*; 
+
+class TCPServer { 
+
+	public static void main(String argv[]) throws Exception 
+	{ 
+		String clientSentence = ""; 
+		String capitalizedSentence; 
+		ServerSocket welcomeSocket = new ServerSocket(6789); 
+
+		while(true) { 
+				Socket connectionSocket = welcomeSocket.accept(); 
+
+				BufferedReader inFromClient = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream())); 
+
+				DataOutputStream  outToClient = new DataOutputStream(connectionSocket.getOutputStream()); 
+
+				clientSentence = inFromClient.readLine(); 
+
+				capitalizedSentence = clientSentence.toUpperCase() + '\n'; 
+
+				outToClient.writeBytes(capitalizedSentence);
+				System.out.println("From Client:" + clientSentence);
+			} 
+		} 
+}
